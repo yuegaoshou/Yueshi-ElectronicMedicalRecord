cc16a8d75b8862ff000ff91e2aa31c71e1d31a73
1493914833@qq.com <1493914833@qq.com>
2021-04-05 20:58:36 +0800

    登录与注册

---
+++ ElectronicMedicalRecord/WindowsFormsApp1/SignIn.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_SignIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb_AccountNum = new System.Windows.Forms.TextBox();
+            this.txb_Password = new System.Windows.Forms.TextBox();
+            this.btn_Register = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_SignIn
+            // 
+            this.btn_SignIn.Location = new System.Drawing.Point(263, 465);
+            this.btn_SignIn.Name = "btn_SignIn";
+            this.btn_SignIn.Size = new System.Drawing.Size(121, 44);
+            this.btn_SignIn.TabIndex = 0;
+            this.btn_SignIn.Text = "登录";
+            this.btn_SignIn.UseVisualStyleBackColor = true;
+            this.btn_SignIn.Click += new System.EventHandler(this.btn_SignIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码：";
+            // 
+            // txb_AccountNum
+            // 
+            this.txb_AccountNum.Location = new System.Drawing.Point(393, 233);
+            this.txb_AccountNum.Name = "txb_AccountNum";
+            this.txb_AccountNum.Size = new System.Drawing.Size(218, 35);
+            this.txb_AccountNum.TabIndex = 3;
+            this.txb_AccountNum.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txb_Password
+            // 
+            this.txb_Password.Location = new System.Drawing.Point(393, 366);
+            this.txb_Password.Name = "txb_Password";
+            this.txb_Password.PasswordChar = '*';
+            this.txb_Password.Size = new System.Drawing.Size(219, 35);
+            this.txb_Password.TabIndex = 4;
+            this.txb_Password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btn_Register
+            // 
+            this.btn_Register.Location = new System.Drawing.Point(528, 465);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(105, 44);
+            this.btn_Register.TabIndex = 5;
+            this.btn_Register.Text = "注册";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.未标题_1;
+            this.pictureBox1.Location = new System.Drawing.Point(174, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(718, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 583);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Register);
+            this.Controls.Add(this.txb_Password);
+            this.Controls.Add(this.txb_AccountNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_SignIn);
+            this.Name = "SignIn";
+            this.Text = "电子病历系统";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SignIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb_AccountNum;
+        private System.Windows.Forms.TextBox txb_Password;
+        private System.Windows.Forms.Button btn_Register;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ElectronicMedicalRecord/WindowsFormsApp1/SignIn.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class SignIn : Form
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            if (this.txb_AccountNum.Text.Trim() == "")
+            {
+                MessageBox.Show("注册账号不能为空！");
+                this.txb_AccountNum.Focus();
+                return;
+            }
+            if (this.txb_Password.Text.Trim() == "")
+            {
+                MessageBox.Show("注册密码不能为空！");
+                this.txb_Password.Focus();
+                return;
+            }
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString =
+                ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText =
+                "INSERT tb_User (No,Password) VALUES (@No,HASHBYTES('MD5',@Password));";
+            sqlCommand.Parameters.AddWithValue("@No", this.txb_AccountNum.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Password", this.txb_Password.Text.Trim());
+            sqlCommand.Parameters["@Password"].SqlDbType = SqlDbType.VarChar;
+            int rowAffected = 0;
+            try
+            {
+                sqlConnection.Open();
+                rowAffected = sqlCommand.ExecuteNonQuery();
+            }
+            catch (SqlException sqlEx)
+            {
+                if (sqlEx.Number==2627)
+                {
+                    MessageBox.Show("您注册的账号已经存在，请重新输入！\n");
+                }
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            if (rowAffected==1)
+            {
+                MessageBox.Show("注册成功！");
+            }
+            else
+            {
+                MessageBox.Show("注册失败！");
+            }
+        }
+
+        private void btn_SignIn_Click(object sender, EventArgs e)
+        {
+            if (this.txb_AccountNum.Text.Trim()=="")
+            {
+                MessageBox.Show("登录账号不能为空！");
+                this.txb_AccountNum.Focus();
+                return;
+            }
+            if (this.txb_Password.Text.Trim()=="")
+            {
+                MessageBox.Show("登录密码不能为空！");
+                this.txb_Password.Focus();
+                return;
+            }
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString =
+                ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText =
+                "SELECT COUNT(1) FROM tb_User WHERE No=@No AND Password=HASHBYTES('MD5',@Password);";
+            sqlCommand.Parameters.AddWithValue("@No", this.txb_AccountNum.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@Password", this.txb_Password.Text.Trim());
+            sqlCommand.Parameters["@Password"].SqlDbType = SqlDbType.VarChar;
+            sqlConnection.Open();
+            int rowAffected = (int)sqlCommand.ExecuteScalar();
+            sqlConnection.Close();
+            if (rowAffected == 1) 
+            {
+                MessageBox.Show("登录成功！");
+            }
+            else
+	       {
+                MessageBox.Show("登录失败！");        
+            }
+            
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
